65fa85f5d889f0f2853d89bd8b359cd5286ff45e
James Doppee <DoppeeJR19@pwcs-edu.org>
2019-01-08 13:56:43 -0500

    Adjusted from haskell2

--- manipulation.hs
+++ manipulation.hs
@@ -9,3 +9,7 @@ stringNumb s x = if mod x 2 == 0
 stringB :: [String]->[String]
 
 stringB xs = [stringNumb x y |(x, y) <-zip xs [0..]]
+
+This is my haskell code
+
+This has just been edited from haskell2 Branch
